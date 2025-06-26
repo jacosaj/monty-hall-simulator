8b956df225c1872b4cc383e41f5f9a5028e723a2
jacosaj <jacekszacek@gmail.com>
2025-06-26 14:39:13 +0200

    fix: updated main

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MontyHallSimulator from './MontyHallSimulator.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MontyHallSimulator />
+  </React.StrictMode>,
+)
