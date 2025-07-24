0a3c2c5575d710a50be93e16f8abf509b8a3b51e
jacosaj <jacekszacek@gmail.com>
2025-07-24 17:48:37 +0200

    fix: files and doubled dir

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"        # RG z kroku 0.5
-    storage_account_name = "montyhalltfstate" # SA z kroku 0.5
+    storage_account_name = "montyhalltfstate10648" # SA z kroku 0.5
     container_name       = "tfstate"          # utworzyłeś w 0.5
     key                  = "monty-hall-simulator.tfstate"
   }
